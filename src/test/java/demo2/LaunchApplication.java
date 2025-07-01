3abee400c1851b73e7094c23a9734943e4fdc631
sahoo <sahoo@DESKTOP-0H5R4VD>
2025-07-02 00:59:19 +0530

    Second commit

---
+++ src/test/java/demo2/LaunchApplication.java
@@ -0,0 +1,14 @@
+package demo2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchApplication {
+	@Test
+	public void openApp() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+	}
+
+}
