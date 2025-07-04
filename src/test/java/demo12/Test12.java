75394059bd552bfa4a636a3c8cc994fe79bbec42
sahoo <sahoo@DESKTOP-0H5R4VD>
2025-07-04 14:06:03 +0530

    changes done again

---
+++ src/test/java/demo12/Test12.java
@@ -0,0 +1,13 @@
+package demo12;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test12 {
+	@Test
+	public void demo12() {
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
